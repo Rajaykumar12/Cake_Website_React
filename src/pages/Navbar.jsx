a66593a96c15dcf7cc79d84ebc2f0322e57a484d
Manas M Salian <147305419+Manas-salian@users.noreply.github.com>
2024-11-13 14:26:46 +0530

    css changes

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -7,16 +7,18 @@ export default function Navbar() {
     <>
       <div>
         <h1 className='main'>CAKEIFBUY</h1>
-          <ul className="mainNav">
-              <li><Link className='subnav' to="/">Home</Link></li>
-              <li><Link className='subnav' to="/pages/Contact">Contact</Link></li>
-              <li className='subnav'>
+          <nav>
+          <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/pages/Contact">Contact</Link></li>
+              <li>
                 <div id="search">
                   <input id="search_bar" type="text" placeholder="Search.."></input>
                   <input type="submit" id="submit"></input>
                 </div>
               </li>
           </ul>
+          </nav>
       </div>
     </>
   )
